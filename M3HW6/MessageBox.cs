3307a0aae51913033e3aa5ae7bc2286d7dbe5ddf
Denys <86122979+Swartalfheim@users.noreply.github.com>
2022-12-14 15:18:21 +0200

    add code

---
+++ M3HW6/MessageBox.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M3HW6
+{
+    public class MessageBox
+    {
+        public MessageBox()
+        {
+            Closed = state => { return state; };
+        }
+
+        public delegate State ClosedState(State state);
+        public event ClosedState Closed;
+        public enum State
+        {
+            Ok,
+            Cancel
+        }
+
+        public async Task<State> Open()
+        {
+            Console.WriteLine("Window is open");
+            await Task.Delay(3000);
+            Console.WriteLine("Window is closed");
+            Random rnd = new Random();
+            if (rnd.Next(1, 3) % 2 == 0)
+            {
+                return Closed.Invoke(State.Ok);
+            }
+            else
+            {
+                return Closed.Invoke(State.Cancel);
+            }
+        }
+    }
+}
--- M3HW6/Program.cs
+++ M3HW6/Program.cs
@@ -1,10 +1,19 @@
 ﻿namespace M3HW6
 {
-    internal class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MessageBox messageBox = new MessageBox();
+            Task<MessageBox.State> state = messageBox.Open();
+            if (state.Result == MessageBox.State.Ok)
+            {
+                Console.WriteLine("Operation was accepted");
+            }
+            else if (state.Result == MessageBox.State.Cancel)
+            {
+                Console.WriteLine("Operation was cancelled");
+            }
         }
     }
 }
